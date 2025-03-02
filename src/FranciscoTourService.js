5b93e17d7d7fd7250b96db4919db99529e1c0492
Marcus Costa <marcusvinicius2097@gmail.com>
2025-03-02 11:43:21 -0300

    config axio teste

--- src/FranciscoTourService.js
+++ src/FranciscoTourService.js
@@ -1,21 +1,22 @@
 import axios from "axios";
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 export async function getDadoAno(anoSelecionado){
-    const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/reservavalormes/${anoSelecionado}`);
+    const response = await axios.get(`/reservavalormes/${anoSelecionado}`);
     console.log(response);
     return response.data;
 }
 
 export async function getDadoMesAtual(){
-    const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/reservavalormesatual`);
+    const response = await axios.get(`/reservavalormesatual`);
     return response.data;
 }
 
 export async function getDadoQuantidade(anoSelecionado){
-    const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/reservaquantidade/${anoSelecionado}`);
+    const response = await axios.get(`/reservaquantidade/${anoSelecionado}`);
     return response.data;
 }
 export async function getQuantidadeAtua(){
-    const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/reservaquantidadeatual`);
+    const response = await axios.get(`/reservaquantidadeatual`);
     return response.data;
 }
