8560fe6ee0cdeabfc1e6231e76902069ec3da885
Marcus Costa <marcusvinicius2097@gmail.com>
2025-03-02 22:38:27 -0300

    Add BAse URL

--- src/FranciscoTourService.js
+++ src/FranciscoTourService.js
@@ -1,78 +1,80 @@
 import axios from "axios";
-axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL
+  });
 
 //gets
 export async function getClientes(){
-    const response = await axios.get(`/clientes`);
+    const response = await instance.get(`/clientes`);
     return response.data;
 }
 
 export async function getDadoAno(anoSelecionado){
-    const response = await axios.get(`/reservavalormes/${anoSelecionado}`);
+    const response = await instance.get(`/reservavalormes/${anoSelecionado}`);
     return response.data;
 }
 
 export async function getDadoMesAtual(){
-    const response = await axios.get(`/reservavalormesatual`);
+    const response = await instance.get(`/reservavalormesatual`);
     return response.data;
 }
 
 export async function getDadoQuantidade(anoSelecionado){
-    const response = await axios.get(`/reservaquantidade/${anoSelecionado}`);
+    const response = await instance.get(`/reservaquantidade/${anoSelecionado}`);
     return response.data;
 }
 
 export async function getDataDiferentes(month, year){
-    const response = await axios.get(`/dataDiferentes/${(month + 1)}/${year}`);
+    const response = await instance.get(`/dataDiferentes/${(month + 1)}/${year}`);
     return response.data;
 }
 
 export async function getEstorno(){
-    const response = await axios.get(`/estorno`);
+    const response = await instance.get(`/estorno`);
     return response.data;
 }
 
 export async function getPagamentoReservaAnual(anoSelecionado){
-    const response = await axios.get(`/pagamentoReservaAnual/${anoSelecionado}`);
+    const response = await instance.get(`/pagamentoReservaAnual/${anoSelecionado}`);
     return response.data;
 }
 export async function getPagamentoReservaMensal(month, year){
-    const response = await axios.get(`/pagamentoReservaMensal/${month + 1}/${year}`);
+    const response = await instance.get(`/pagamentoReservaMensal/${month + 1}/${year}`);
     return response.data;
 }
 
 export async function getPagamentoReservaValorMes(anoSelecionado){
-    const response = await axios.get(`/pagamentoreservavalormes/${anoSelecionado}`);
+    const response = await instance.get(`/pagamentoreservavalormes/${anoSelecionado}`);
     return response.data;
 }
 
 export async function getQuantidadeAtua(){
-    const response = await axios.get(`/reservaquantidadeatual`);
+    const response = await instance.get(`/reservaquantidadeatual`);
     return response.data;
 }
 
 export async function getReservas(){
-    const response = await axios.get(`/reservas`);
+    const response = await instance.get(`/reservas`);
     return response.data;
 }
 
 export async function getPagamentoReservas(){
-    const response = await axios.get(`/reservaPagamento`);
+    const response = await instance.get(`/reservaPagamento`);
     return response.data;
 }
 
 export async function getTours(){
-    const response = await axios.get(`/tour`);
+    const response = await instance.get(`/tour`);
     return response.data;
 }
 
 export async function getTourPorMes(month, year){
-    const response = await axios.get(`/tourPorMes/${(month + 1)}/${year}`);
+    const response = await instance.get(`/tourPorMes/${(month + 1)}/${year}`);
     return response.data;
 }
 
 export async function getTiposTours(){
-    const response = await axios.get(`/tiposTours`);
+    const response = await instance.get(`/tiposTours`);
     return response.data;
 }
 
